992f499c2ee6f552928440df2e2aebfb728df738
Hasan Kadir Tosun <hktosun@gmail.com>
2017-12-21 23:12:46 -0600

    update

--- coursework/IO/ECON8602/hw4.R
+++ coursework/IO/ECON8602/hw4.R
@@ -11,3 +11,12 @@ data$bxt1 <- as.numeric(cut(data$xt1, seq(-0.001,450000,5000), labels = 1:90,inc
 data <- data %>% mutate(xtchange = (i==0)*(bxt1-bxt) + (i==1)*(bxt1-1))
 data <- data %>% mutate(frag = (xtchange<=2)*xtchange + (xtchange >2)*2)
 probs <- data %>% group_by(frag) %>% summarize(m = n()/nrow(data))
+pi = as.array(probs$m)
+write_csv(data,"rust.csv", col_names = TRUE)
+
+pdx <- data %>% group_by(bxt) %>% summarize(n = sum(i==0)/n())
+pdx
+
+ggplot(pdx,aes(bxt)) + 
+  geom_line(aes(y = n)) +
+  labs(title = "pdf of log wages in the first spell", x = "Wage", y = "Cumulative density")
